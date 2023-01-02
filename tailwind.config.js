717eb44ecd931bf4c3e2287957c3af3e3d1b805e
Yash <bajajy82@gmail.com>
2023-01-02 18:49:49 +0530

    removed native theme and added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./App.tsx","./components/**/*.{ts,tsx}","./screens/**/*.{ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
